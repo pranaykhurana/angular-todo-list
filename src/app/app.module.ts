2a8183c579ccf7e3768c95e4cacd0fc447c02154
pranay <pranay.khurana99@gmail.com>
2022-02-16 23:44:08 +1100

    Implemented edit and delete functionality added dialog to edit tooltips using tippy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 import {MatIconModule} from "@angular/material/icon";
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import { ToolTipDirective } from './shared/tool-tip.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent,
+    EditTodoDialogComponent,
+    ToolTipDirective
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        MatIconModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatIconModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Todo} from "../shared/todo.model";
 import {DataService} from "../shared/data.service";
 import {NgForm} from "@angular/forms";
+import {MatDialog} from "@angular/material/dialog";
+import {EditTodoDialogComponent} from "../edit-todo-dialog/edit-todo-dialog.component";
 
 @Component({
   selector: 'app-todos',
@@ -10,10 +12,11 @@ import {NgForm} from "@angular/forms";
 })
 export class TodosComponent implements OnInit {
 
-  todos: Todo[] | undefined;
+  todos: Todo[];
   showValidationErrors: boolean = false;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+              private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.todos = this.dataService.getAllTodos();
@@ -28,4 +31,28 @@ export class TodosComponent implements OnInit {
     }
   }
 
+  toggleCompleted(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  editTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    let dialogRef = this.dialog.open(EditTodoDialogComponent, {
+      width: '700px',
+      data: todo
+    });
+
+    dialogRef.afterClosed().subscribe((res) => {
+      if (res) {
+        console.log("in closed", res);
+        this.dataService.updateTodo(index, res);
+      }
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    this.dataService.deleteTodo(index);
+  }
 }
